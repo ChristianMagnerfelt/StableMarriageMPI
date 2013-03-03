e958114df2011bae7decaa4ed6879b778401bfd6
Christian Magnerfelt <christian.magnerfelt@gmail.com>
2013-03-03 20:06:58 +0100

    Added loop for male to propose and receive rejection. Loop is also able to receive a message about the number of women that have accepted

--- stable_marriage_mpi.cpp
+++ stable_marriage_mpi.cpp
@@ -7,6 +7,7 @@
 
 int g_numProcs = 0;
 int g_myId = 0;
+int g_numAccepted = 0;
 
 std::vector<int> g_rank;
 
@@ -28,14 +29,47 @@ int main(int argc, const char * argv[])
 	if(g_myId > g_numProcs / 2)
 	{
 		std::cout << "I'm nr " << g_myId << " and I'm a man" << std::endl;
-	initRank(g_rank);
+		initRank(g_rank);
 
+		int buffer [2];
+		bool matched;
+		MPI_Status status;
+
+		for(std::size_t i = 0; i < g_rank.size(); ++i)
+		{
+			matched = false;
+			buffer[0] = 1;
+			buffer[1] = g_myId;
+			MPI_Send(buffer, 2, MPI_INT, g_rank[i], 0, MPI_COMM_WORLD);
+			while(matched)
+			{
+				MPI_Recv(buffer, 2, MPI_INT, g_rank[i], 0, MPI_COMM_WORLD, &status);
+				if(buffer[0] == 0)
+				{
+					++g_numAccepted;
+					if(g_numAccepted == g_rank.size())
+					{
+						return 0;
+					}
+				}
+				else
+				{
+					if(buffer[1] == 0)
+						matched = false;
+				}
+			}
+		}
 	}
 	else
 	{
 		std::cout << "I'm nr " << g_myId << " and I'm a woman" << std::endl;
-	initRank(g_rank);
+		initRank(g_rank);
 
+		bool done = false;
+		while(!done)		
+		{
+
+		}
 	}
 	MPI_Finalize();
 	return 0;
